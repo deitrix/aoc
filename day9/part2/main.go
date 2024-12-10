fd15b2b2d7855df1e698d19ff3194ecad36a8097
Big Phil <deitrix@users.noreply.github.com>
2024-12-10 13:03:08 +0000

    Day 9

--- day9/part2/main.go
+++ day9/part2/main.go
@@ -4,14 +4,13 @@ import (
 	"bytes"
 	"fmt"
 	"io"
-	"os"
 	"slices"
 	"strings"
 
 	"github.com/deitrix/aoc/day9"
 )
 
-var input = day9.Example
+var input = day9.Input
 
 func main() {
 	var blocks []Block
@@ -20,34 +19,73 @@ func main() {
 		size -= 48
 		switch i % 2 {
 		case 0:
-			// file block
 			blocks = append(blocks, Block{Type: FileBlock, Size: int(size), FileID: id})
 			id++
 		case 1:
-			// free block
 			blocks = append(blocks, Block{Type: FreeBlock, Size: int(size)})
 		}
 	}
-	Render(os.Stdout, blocks)
-	fmt.Println()
+
 	fileIndex := len(blocks)
-	freeIndex := 0
-	for freeIndex < fileIndex {
-		if fileIndex == 0 {
+	for {
+		fileIndex = nextFileBlock(blocks[:fileIndex])
+		if fileIndex == -1 {
 			break
 		}
-		for i, b := range slices.Backward(blocks[:fileIndex]) {
-			if b.Type == FileBlock {
-				fileIndex = i
-				break
-			}
+		freeIndex := nextFreeBlock(blocks[:fileIndex], blocks[fileIndex].Size)
+		if freeIndex == -1 {
+			continue
 		}
-		for i, b := range blocks[freeIndex:] {
-			if b.Type == FreeBlock {
+		// Convert the file block to a free block
+		blocks[fileIndex].Type = FreeBlock
 
-			}
+		// If the blocks are the same size, we can just change the type and copy over the file ID
+		if blocks[fileIndex].Size == blocks[freeIndex].Size {
+			blocks[freeIndex].Type = FileBlock
+			blocks[freeIndex].FileID = blocks[fileIndex].FileID
+		} else {
+			// Otherwise, we need to reduce the size of the free block and insert a new file block
+			// before it, making sure to update the fileIndex cursor to account for the new element.
+			blocks[freeIndex].Size -= blocks[fileIndex].Size
+			blocks = slices.Insert(blocks, freeIndex, Block{
+				Type:   FileBlock,
+				Size:   blocks[fileIndex].Size,
+				FileID: blocks[fileIndex].FileID,
+			})
+			fileIndex++
 		}
 	}
+
+	var result int
+	var index int
+	for _, b := range blocks {
+		if b.Type == FileBlock {
+			for i := range b.Size {
+				result += (i + index) * b.FileID
+			}
+		}
+		index += b.Size
+	}
+
+	fmt.Println(result)
+}
+
+func nextFileBlock(blocks []Block) int {
+	for i, b := range slices.Backward(blocks) {
+		if b.Type == FileBlock {
+			return i
+		}
+	}
+	return -1
+}
+
+func nextFreeBlock(blocks []Block, minSize int) int {
+	for i, b := range blocks {
+		if b.Type == FreeBlock && b.Size >= minSize {
+			return i
+		}
+	}
+	return -1
 }
 
 func Render(w io.Writer, blocks []Block) {
